a7a302977b33256d0ff6a7c055745104b144c6ee
Gauravguptas <160850727+Gauravguptas@users.noreply.github.com>
2024-03-08 21:11:58 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
-sudo_groups = -1001777454744
+api_id = 28498858
+api_hash = "6cc3cfdeae65b6ae7f47fb828edf38f4"
+bot_token = "7065990270:AAGWWx42WHw8Q_G4TMtuWe4AgID0E1d3XfM"
+sudo_groups = -1002127933724
 
